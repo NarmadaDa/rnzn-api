718ee9b0ea9080823d224aa8a401d17095d94453
Narmada <narmada@villvay.com>
2022-01-31 21:22:45 +0530

    Upload file -fixed

--- src/app/Http/Controllers/Media/Services/MediaService.php
+++ src/app/Http/Controllers/Media/Services/MediaService.php
@@ -12,41 +12,21 @@ class MediaService {
     protected $azureAccountKey;
 
     public function __construct() {
-        $this->azureAccountName = config('queue.connections.azure.accountname');
-        $this->azureAccountKey = config('queue.connections.azure.key');
+        
     }
 
 
     public function uploadToStorage(UploadedFile $file) {
-        $date = Carbon::now()->format('Y/m');
-        $filename = Str::uuid()->toString(); 
+        $date = Carbon::now()->format('Y-m');
+        $file_name = Str::uuid()->toString(); 
 
-        // // Create blob client.
-        // $blobClient = BlobRestProxy::createBlobService("DefaultEndpointsProtocol=http;AccountName={$this->azureAccountName};AccountKey={$this->azureAccountKey};EndpointSuffix=core.windows.net"); 
-
-
-
-        $fileName = $date.'_'.$file->getClientOriginalName();
+        $fileName = $date.$file_name.$file->getClientOriginalName();
         // save file to azure blob virtual directory uplaods in your container
-        $filePath = $file->storeAs('photos/', $fileName, 'azure');
+        $filePath = $file->storeAs('uploads', $fileName, 'azure'); 
 
-
-
-        // $fileName = time().'_'.$req->file->getClientOriginalName();
-        // // save file to azure blob virtual directory uplaods in your container
-        // $filePath = $req->file('file')->storeAs('uploads/', $fileName, 'azure');
-
-    
-        // $options = new CreateBlockBlobOptions;
-        // $options->setContentType($file->getClientMimeType());  
-        // $filePath = "$date/$filename.{$file->guessClientExtension()}";
-
-
-
-        // $blobClient->createBlockBlob("photos", $filePath , file_get_contents($file->getPathname()), $options); 
         return [
-            "file_url" => "http://{$this->azureAccountName}.blob.core.windows.net/photos/$filePath",
-            "relative_file_url" => "/photos/$filePath"
+            "file_url" => env('AZURE_STORAGE_URL') . env('AZURE_STORAGE_CONTAINER') . '/' . "$filePath",
+            "relative_file_url" => "$filePath"
         ];
     }
 }
