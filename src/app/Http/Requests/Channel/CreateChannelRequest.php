80090d6390b957cbb5b41a20bf323bee17f28d28
Narmada <narmada@villvay.com>
2022-03-11 13:10:55 +0530

    add channel with migrations

---
+++ src/app/Http/Requests/Channel/CreateChannelRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Channel;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateChannelRequest extends FormRequest
+{
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      'name'            => 'required|string',
+      'initial_post'    => 'nullable',
+      'post_pin'        => 'nullable', 
+      'channel_active'  => 'nullable',   
+      'image'           => 'nullable', 
+    ];
+  }
+
+  /**
+   * Get the error messages for the defined validation rules.
+   *
+   * @return array
+   */
+  public function messages()
+  {
+    return [
+      'name.required'    => 'Channel name is required.',
+      'name.string'      => 'Invalid channel name.',  
+    ];
+  }
+}
