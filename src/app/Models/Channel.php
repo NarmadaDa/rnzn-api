4cdaee83686fc661aa231d5226e5b31f4df7d688
Narmada <narmada@villvay.com>
2022-03-28 15:17:51 +0530

    fixed reaction array

--- src/app/Models/Channel.php
+++ src/app/Models/Channel.php
@@ -42,22 +42,25 @@ class Channel extends UUIDModel
     $posts = DB::table('channels AS c')
       ->join('forum_posts AS fp', 'c.id', '=', 'fp.channel_id')  
       ->join('profiles AS p', 'p.user_id', '=', 'fp.user_id')
-      ->join('users AS u', 'fp.user_id', '=', 'u.id')   
+      ->join('users AS u', 'fp.user_id', '=', 'u.id')  
+      
+      
+      // ->join('forum_post_reactions AS r', 'r.post_id', '=', 'fp.id') 
 
       ->select('fp.id', 'fp.channel_id AS channelId', 'fp.uuid AS postUuid','fp.post', 'fp.pin_post AS pinPost', "fp.created_at AS createdAt", "fp.updated_at AS updatedAt",
-      'p.first_name AS firstName', 'p.middle_name AS middleName', 'p.last_name AS lastName', 'p.image', 'u.uuid AS userUUID',  
+      'p.first_name AS firstName', 'p.middle_name AS middleName', 'p.last_name AS lastName', 'p.image', 'u.uuid AS userUUID'
       
-      DB::raw("(SELECT GROUP_CONCAT(uuid) FROM forum_post_reactions WHERE likes = 1)  as like_users"),
-      DB::raw("(SELECT GROUP_CONCAT(uuid) FROM forum_post_reactions WHERE haha = 1)  as haha_users"),
-      DB::raw("(SELECT GROUP_CONCAT(uuid) FROM forum_post_reactions WHERE wow = 1)  as wow_users"),
-      DB::raw("(SELECT GROUP_CONCAT(uuid) FROM forum_post_reactions WHERE sad = 1)  as sad_users"),
-      DB::raw("(SELECT GROUP_CONCAT(uuid) FROM forum_post_reactions WHERE angry = 1)  as angry_users")
+      // DB::raw("(SELECT GROUP_CONCAT(uuid) FROM forum_post_reactions WHERE likes = 1)  as like_users"),
+      // DB::raw("(SELECT GROUP_CONCAT(uuid) FROM forum_post_reactions WHERE haha = 1)  as haha_users"),
+      // DB::raw("(SELECT GROUP_CONCAT(uuid) FROM forum_post_reactions WHERE wow = 1)  as wow_users"),
+      // DB::raw("(SELECT GROUP_CONCAT(uuid) FROM forum_post_reactions WHERE sad = 1)  as sad_users"),
+      // DB::raw("(SELECT GROUP_CONCAT(uuid) FROM forum_post_reactions WHERE angry = 1)  as angry_users")
       )  
 
       ->where('c.id', '=', $id)
       ->where('c.channel_active', '=', 1)
       ->orderBy('pinPost', 'desc')
-      ->orderBy('createdAt', 'asc')
+      ->orderBy('createdAt', 'asc') 
       ->get();   
  
       return $posts;
