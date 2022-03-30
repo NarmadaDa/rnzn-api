cb879421d07bde059d79438619dee44bcc994063
Narmada <narmada@villvay.com>
2022-03-31 03:15:25 +0530

    Added only admin can pin a post-fixed

--- src/app/Http/Controllers/Channel/PinPost.php
+++ src/app/Http/Controllers/Channel/PinPost.php
@@ -22,54 +22,52 @@ class PinPost extends BaseChannelController
     
     $data = $request->validated(); 
     $user = $request->user();  
-
+ 
     if($user->userRole->role_id != 1 || $user->userRole->role_id != 2 ){
+
       abort(404, "Only Admin users can pin a post.");
+
     }
     
     $post = $this->pinpostRepository->findByUUID($data["uuid"]);
     if (!$post) {
+
       abort(404, "Post does not exist.");
+
     }
-    
-    $post_by_user = $this->pinpostRepository->findByUserID($data["uuid"], $user->id);
-    if (!$post_by_user) {
-      abort(404, "Post does not exist valid user.");
-    }
+
+    // Pin a post
+    if ($data["type"] != "pin") {
+
+      abort(404, "Type should be 'pin'");
+
+    } 
     
     DB::beginTransaction();
     
-    try {   
-      
-      // Pin a post
-      if ($data["type"] == "pin") {
+    try {    
     
         $pinned_posts = $this->pinpostRepository->pinned_posts();
         
         //Make other pinned posts in same channel to false and pin only one post per channel
         //Iterate through pinned posts and find if there are same channel IDs
-        
+
         for($x = 0; $x < count($pinned_posts) - 1; $x++){
 
-          if($pinned_posts[$x] -> channel_id == $post -> channel_id){
+          if($pinned_posts[$x]->channel_id == $post->channel_id){
 
-            $pinned_posts[$x] -> pin_post = 0;
-            $pinned_posts[$x] -> save();
+            $pinned_posts[$x]->pin_post = 0;
+            $pinned_posts[$x]->save();
             
           }              
           
         }
+        
         //Pin current post given by uuid
         $post->pin_post = 1; 
         $post->save();
       
         $message  = "Current post pinned successfully.";
-        
-      } else {
-        
-        abort(404, "Type should be 'pin'");
-        
-      }
       
     } catch (Exception $e) {
       DB::rollback();
