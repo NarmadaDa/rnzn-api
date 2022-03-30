bca1ba47de06452038e516e58e4ad99fb5d29c80
Narmada <narmada@villvay.com>
2022-03-30 16:58:32 +0530

    pin post-check admin

--- src/app/Http/Controllers/Channel/PinPost.php
+++ src/app/Http/Controllers/Channel/PinPost.php
@@ -21,14 +21,21 @@ class PinPost extends BaseChannelController
   { 
     
     $data = $request->validated(); 
-    $user_id = $request->user()->id;  
+    $user = $request->user();  
+
+    if($user->userRole->role_id != 1 || $user->userRole->role_id != 2 ){
+      abort(404, "Only Admin users can pin a post.");
+    }
     
     $post = $this->pinpostRepository->findByUUID($data["uuid"]);
     if (!$post) {
       abort(404, "Post does not exist.");
     }
     
-    $pinned_posts = $this->pinpostRepository->pinned_posts();
+    $post_by_user = $this->pinpostRepository->findByUserID($data["uuid"], $user->id);
+    if (!$post_by_user) {
+      abort(404, "Post does not exist valid user.");
+    }
     
     DB::beginTransaction();
     
@@ -36,11 +43,16 @@ class PinPost extends BaseChannelController
       
       // Pin a post
       if ($data["type"] == "pin") {
+    
+        $pinned_posts = $this->pinpostRepository->pinned_posts();
         
         //Make other pinned posts in same channel to false and pin only one post per channel
         //Iterate through pinned posts and find if there are same channel IDs
+        
         for($x = 0; $x < count($pinned_posts) - 1; $x++){
+
           if($pinned_posts[$x] -> channel_id == $post -> channel_id){
+
             $pinned_posts[$x] -> pin_post = 0;
             $pinned_posts[$x] -> save();
             
@@ -50,11 +62,10 @@ class PinPost extends BaseChannelController
         //Pin current post given by uuid
         $post->pin_post = 1; 
         $post->save();
-     
-        //return["pinned_posts" => ($pinned_posts)];
+      
         $message  = "Current post pinned successfully.";
         
-      }else {
+      } else {
         
         abort(404, "Type should be 'pin'");
         
@@ -72,64 +83,5 @@ class PinPost extends BaseChannelController
     ];
   }
   
-  
 }
-
-
-// namespace App\Http\Controllers\Channel;
-
-// use App\Http\Controllers\Channel\BaseChannelController; 
-// use App\Http\Requests\Channel\CreatePostRequest; 
-// use App\Models\ForumPost; 
-// use App\Models\PostType; 
-// use Illuminate\Support\Arr;
-// use Exception;
-// use Illuminate\Support\Facades\DB;
-
-// class PinPost extends BaseChannelController
-// {
-//   /**
-//    * Handle the incoming request.
-//    *
-//    * @return \Illuminate\Http\Response
-//    */ 
-
-//   public function __invoke(CreatePostRequest $request)
-//   { 
-
-//     $data = $request->validated(); 
-//     $user_id = $request->user()->id;  
-//     $user_uuid = $request->user()->uuid;  
-
-//     $channel = $this->channelRepository->findByID($data["channel_id"]);
-//     if (!$channel) {
-//       abort(404, "Channel does not exist.");
-//     }
-
-//     DB::beginTransaction();
-
-//     try {   
- 
-//       // insert data to "forum_posts"
-//       $form_post_id = $this->formpostRepository->create([
-//          "post" => $data["post"], 
-//          "channel_id" => $data["channel_id"], 
-//          "user_id" => $user_id,
-//       ]);   
-
-//     } catch (Exception $e) {
-//         DB::rollback();
-//         abort(500, $e->getMessage());
-//     }
-
-//     DB::commit();
-
-//     return [
-//       "message"   => "Post successfully created.",
-//       "post_uuid" => $form_post_id->uuid
-//     ];
-//   }
-
-
-// }
  
